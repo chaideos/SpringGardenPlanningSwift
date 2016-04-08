20ee2255f200f247194c8ae795a9db1940e0b9d4
chaideos <chaitanya.deos@gmail.com>
2016-04-08 11:17:18 -0700

    Initial Commit

---
+++ SpringGardenPlanningTests/SpringGardenPlanningTests.swift
@@ -0,0 +1,36 @@
+//
+//  SpringGardenPlanningTests.swift
+//  SpringGardenPlanningTests
+//
+//  Created by Chaitanya Deosthale on 4/8/16.
+//  Copyright © 2016 Chaitanya Deosthale. All rights reserved.
+//
+
+import XCTest
+@testable import SpringGardenPlanning
+
+class SpringGardenPlanningTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
